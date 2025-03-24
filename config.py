eae62f4d222b38c369cfc03637662556b180de70
emiranivala <taitufam@gmail.com>
2025-03-24 21:31:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "22642292"))
 API_HASH = getenv("API_HASH", "4502d35191a2fcb02c8467f54789f0ea")
-BOT_TOKEN = getenv("BOT_TOKEN", "8048330196:AAHQSSa8IijT0HfzOEIk40pD5nX1NNU2eas")  # Replace with your actual bot token from @BotFather
+BOT_TOKEN = getenv("BOT_TOKEN", "")  # Replace with your actual bot token from @BotFather
 OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb://localhost:27017/telegram_bot")
+MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "-1002493565037")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002329264016"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "20"))
